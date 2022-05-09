e29716a11b60b1b6214980346e9ffa4e5e868e39
MurilloAssis <murillo98asiss@gmail.com>
2022-05-09 16:03:24 -0300

    Upload de arquivo finalizado

--- Utils/Upload.cs
+++ Utils/Upload.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.IO;
 using System.Net.Http.Headers;
@@ -7,15 +8,25 @@ namespace SenaiRH_G1.Utils
 {
     public static class Upload
     {
+        private const string STRING_DE_CONEXAO = "DefaultEndpointsProtocol=https;AccountName=armazenamentogrupo3;AccountKey=Y4K/lMSydo5BhOrGW1NdiyLYWJdqHsm6ohUG9SWvEGJeZmxWPbmjy6DrGYlJgIqn6ADyIH/gAfaKF1NgTQ391Q==;EndpointSuffix=core.windows.net";
+        private const string BLOB_CONTAINER_NAME = "amazenamento-simples-grp1";
+
+        private static BlobContainerClient BlobContainerClient { get; set; }
+
+        static Upload()
+        {
+            //Permite que manipulemos um container
+            BlobContainerClient = new BlobContainerClient(STRING_DE_CONEXAO, BLOB_CONTAINER_NAME);
+        }
 
         /// <summary>
         /// Faz o upload do arquivo para o servidor
         /// </summary>
         /// <param name="arquivo">Arquivo vindo de um formulário</param>
-        /// <param name="extensoesPermitidas">Array com extensões permitidas apenas</param>
         /// <returns>Nome do arquivo salvo</returns>
-        public static string UploadFile(IFormFile arquivo, string[] extensoesPermitidas)
+        public static string UploadFile(IFormFile arquivo)
         {
+            string[] extensoesPermitidas = { "jpg", "png", "jpeg", "pdf" };
             try
             {
                 var pasta = Path.Combine("StaticFiles", "Images");
@@ -31,10 +42,11 @@ namespace SenaiRH_G1.Utils
                         var novoNome = $"{Guid.NewGuid()}.{extensao}";
                         var caminhoCompleto = Path.Combine(caminho, novoNome);
 
-                        using (var stream = new FileStream(caminhoCompleto, FileMode.Create))
-                        {
-                            arquivo.CopyTo(stream);
-                        }
+                        BlobClient blobClient = BlobContainerClient.GetBlobClient(novoNome);
+
+                        //Cria um novo block blob (arquivo)
+                        blobClient.Upload(arquivo.OpenReadStream());
+
                         return novoNome;
                     }
                     return "Extensão não permitida";
@@ -56,6 +68,7 @@ namespace SenaiRH_G1.Utils
         /// <returns>Verdadeiro/Falso</returns>
         public static bool ValidarExtensao(string[] extensoes, string nomeDoArquivo)
         {
+
             string[] dados = nomeDoArquivo.Split(".");
             string extensao = dados[dados.Length - 1];
 
@@ -75,11 +88,50 @@ namespace SenaiRH_G1.Utils
         /// <param name="nomeDoArquivo">Nome do Arquivo</param>
         public static void RemoverArquivo(string nomeDoArquivo)
         {
-            var pasta = Path.Combine("StaticFile", "Images");
-            var caminho = Path.Combine(Directory.GetCurrentDirectory(), pasta);
-            var caminhoCompleto = Path.Combine(caminho,nomeDoArquivo);
+            BlobClient blobClient = BlobContainerClient.GetBlobClient(nomeDoArquivo);
 
-            File.Delete(caminhoCompleto);   
+            blobClient.Delete();
+        }
+
+        /// <summary>
+        /// Atualiza a foto de perfil
+        /// </summary>
+        /// <param name="nomeFotoAntiga">Nome da foto antiga</param>
+        /// <param name="novaFoto">Arquivo da nova foto</param>
+        /// <returns>O nome da nova foto</returns>
+        public static string AtualizarFoto(string nomeFotoAntiga, IFormFile novaFoto)
+        {
+
+            try
+            {
+                //Remove a foto antiga
+                RemoverArquivo(nomeFotoAntiga);
+
+                //Coloca a nova foto que foi inserida
+                string nomeFotoAtualizada = UploadFile(novaFoto);
+
+                // retorna o nome da foto com a guid
+                return nomeFotoAtualizada;
+            }
+            catch (Azure.RequestFailedException azureExecp)
+            {
+                //Pega o status code da requisição
+                string statusCode = azureExecp.Status.ToString();
+
+                if (statusCode == "404")
+                {
+                    string nomeFotoAtualizada = UploadFile(novaFoto);
+
+                    return nomeFotoAtualizada;
+                }
+
+                // Retorna o erro.
+                return azureExecp.ToString();
+            }
+            catch (Exception exp)
+            {
+                return exp.ToString();
+            }
         }
 
         /// <summary>
