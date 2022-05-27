5e8a350c6f21d703b05ffb1d333a94d370715659
MurilloAssis <murillo98asiss@gmail.com>
2022-05-27 14:41:07 -0300

    Alteração dataConclusao

---
+++ ViewModel/arquivoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiRH_G1.ViewModel
+{
+    public class arquivoViewModel
+    {
+        public IFormFile arquivo;
+    }
+}
